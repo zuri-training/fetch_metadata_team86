aaf1fec48167df753c120c2cab0fa7a4a08ec888
Adegite Taiwo <adegitetaiwo24@gmail.com>
2022-08-04 06:39:20 +0100

    improving settings for deployment on heroku

--- backend/fetch_metadata/settings.py
+++ backend/fetch_metadata/settings.py
@@ -10,15 +10,32 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from sentry_sdk.integrations.django import DjangoIntegration
+import sentry_sdk
 from pathlib import Path
 import os
 from dotenv import load_dotenv
 import django_heroku
-
-
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 load_dotenv()  # take environment variables from .env.
 
+sentry_sdk.init(
+    dsn=os.getenv('sentry_sdk_dsn'),
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
