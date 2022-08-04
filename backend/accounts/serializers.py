08e66629da45d67aa96cc34b719dce1c488c13d4
Adegite Taiwo <adegitetaiwo24@gmail.com>
2022-08-05 00:17:22 +0100

    improved

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,3 +1,5 @@
+from urllib import request
+from pkg_resources import require
 from rest_framework.response import Response
 from rest_framework import generics
 from rest_framework import status
@@ -11,6 +13,7 @@ import sys
 from django.core import exceptions
 import django.contrib.auth.password_validation as validators
 
+
 from rest_framework import status
 from rest_framework import generics
 from rest_framework.response import Response
@@ -19,6 +22,8 @@ from rest_framework.permissions import IsAuthenticated
 
 
 class UserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(required=True)
+
     class Meta:
         model = CustomUser
         fields = ['id', 'username', 'email', 'first_name', 'last_name']
@@ -30,6 +35,7 @@ class RegisterUserSerializer(serializers.ModelSerializer):
     creates a user account if all condition is met and returns the user object
     created.
     '''
+    username = serializers.CharField(required=True)
     password = serializers.CharField(write_only=True)
     confirm_password = serializers.CharField(write_only=True)
 
@@ -110,3 +116,38 @@ class ChangePasswordSerializer(serializers.Serializer):
             raise serializers.ValidationError(errors)
           
         return super(ChangePasswordSerializer, self).validate(data)
+
+
+class UpdateUserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(required=False)
+    email = serializers.EmailField(required=False)
+
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'first_name', 'last_name', 'email')
+        
+        
+    def validate(self, data):
+         
+        # get the email and new_email from the data
+        email = data.get('email')
+
+         
+        errors = dict() 
+        try:
+            # validate the password and catch the exception
+            if CustomUser.objects.filter(email=email).exists():
+                raise serializers.ValidationError(
+                    {"email": "This email is already in use."})
+
+
+        # the exception raised here is different than serializers.ValidationError
+        except exceptions.ValidationError as e:
+            errors['password'] = list(e.messages)
+         
+        if errors:
+            raise serializers.ValidationError(errors)
+          
+        return super(UpdateUserSerializer, self).validate(data)
+
+
