42e6ed1e641db02c758bfc18990387d62a27fb6d
coded1guy <grggmmnl@gmail.com>
2022-08-11 23:17:35 +0100

    update

--- frontend/scripts/dashboard-header.js
+++ frontend/scripts/dashboard-header.js
@@ -1,5 +1,8 @@
-const headerDDBtn = document.querySelector("#setting-dropdown");
-const headerDropdown = document.querySelector("#profile-settings-dd");
+const headerDDBtn = document.querySelector("#setting-dropdown"),
+mbHeaderDDBtn = document.querySelector("#profile-set"),
+headerDropdown = document.querySelector("#profile-settings-dd");
+
+const logoutBtn = document.querySelector("#logout-btn");
 
 headerDropdown.style.display = "none";
 
@@ -14,7 +17,19 @@ const toggleHeaderDropdown = () => {
 const closeHeaderDropdown = () => {
     headerDropdown.style.display = "none";
 }
+const openHeaderDropdown = () => {
+    headerDropdown.style.display = "none";
+    console.log("open");
+}
 
-headerDDBtn.onclick = ()=> {
-    toggleHeaderDropdown();
+document.onclick = (e)=> {
+    if(headerDDBtn.contains(e.target) || mbHeaderDDBtn.contains(e.target)) {
+        toggleHeaderDropdown();
+    } else {
+        closeHeaderDropdown();
+    }
+}
+
+logoutBtn.onclick = ()=> {
+    console.log("logout");
 }
