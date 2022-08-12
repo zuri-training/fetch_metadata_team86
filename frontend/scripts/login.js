2ef0f9e26bfd9d9fc2b8908639bac93a15604ccc
coded1guy <grggmmnl@gmail.com>
2022-08-12 02:37:18 +0100

    update

--- frontend/scripts/login.js
+++ frontend/scripts/login.js
@@ -4,6 +4,7 @@ const
 loginForm = document.querySelector("#login-form"),
 email = document.querySelector("#email"),
 password = document.querySelector("#password");
+const loginBtn = document.querySelector("#login-btn");
 
 // const loginData = {
 //   'username': userName.value,
@@ -42,6 +43,12 @@ loginForm.onsubmit = (e)=> {
       window.location.replace("http://127.0.0.1:5500/frontend/Convert_page/convert_file.html");
     } else {
       console.log(result.data);
+      if(result.non_field_errors[0] === "'Unable to log in with provided credentials.'") {
+        email.validity.valid = false;
+        email.setCustomValidity('Unable to log in with provided credentials.');
+        password.setCustomValidity('Unable to log in with provided credentials.');
+        loginBtn.click();
+      }
     }
   })
   .catch(error => console.log(error));
