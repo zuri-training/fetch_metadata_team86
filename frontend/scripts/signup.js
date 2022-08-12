708c71d732aea790390df700459e61c45459b680
coded1guy <grggmmnl@gmail.com>
2022-08-12 02:23:34 +0100

    Update signup.js

--- frontend/scripts/signup.js
+++ frontend/scripts/signup.js
@@ -7,12 +7,48 @@ password = document.querySelector("#password"),
 confirmPassword = document.querySelector("#cpassword"),
 submitBtn = document.querySelector("#submit-register-form");
 
-// const registerationData = {
-//   'username': "stilltesteresossres",
-//   'email': "stilltesterossres@mail.com",
-//   'password': "cvfgdgfhgty565765857",
-//   'confirm_password': "cvfgdgfhgty565765857",
-// }
+let cPassGood = false;
+
+const inputs = document.querySelectorAll("#registeration-form input");
+inputs.forEach((input)=> {
+  input.oninput = (e)=> {
+    e.target.value = e.target.value.trim();
+  }
+  input.onblur = (e)=>{
+    if(e.target.value.split(" ").length > 1) {
+      console.log(true);
+      e.target.value = e.target.value.split(" ").join("");
+      const inputName = e.target.name;
+      e.target.setCustomValidity(`There can't be whitespace between ${inputName}`);
+    }
+  }
+  input.onfocus = (e)=>{
+    e.target.setCustomValidity("");
+  }
+});
+
+confirmPassword.onblur = (e)=> {
+  if(e.target.value !== password.value) {
+    e.target.setCustomValidity(`confirm password doesn't have the same value as password`);
+    cPassGood = false;
+  } else {
+    e.target.setCustomValidity(``);
+    cPassGood = true;
+  }
+}
+password.onblur = (e)=> {
+  if(!Number(e.target.value) && e.target.value.length < 9) {
+    e.target.setCustomValidity(`password must be more than 8 characters`);
+    cPassGood = false;
+  }
+  else if(Number(e.target.value)) {
+    e.target.setCustomValidity(`password cannot be entirely numeric`);
+    cPassGood = false;
+  } else {
+    e.target.setCustomValidity(``);
+    cPassGood = true;
+  }
+}
 
 async function postData(formdata) {
   const { username, email, password, confirm_password } = formdata;
@@ -28,19 +64,28 @@ async function postData(formdata) {
 
 registerForm.onsubmit = (e)=> {
   e.preventDefault();
-  const registerationData = {
-    'username': userName.value,
-    'email': email.value,
-    'password': password.value,
-    'confirm_password': confirmPassword.value,
-  }
-  postData(registerationData).then(result=> {
-    console.log(result);
-    if(result.code === 200) {
-      console.log("success");
-      window.location.replace("http://127.0.0.1:5500/frontend/pages/login.html");
-    } else {
-      console.log("error");
+  if(cPassGood) {
+    console.log("in here");
+    const registerationData = {
+      'username': userName.value,
+      'email': email.value,
+      'password': password.value,
+      'confirm_password': confirmPassword.value,
     }
-  });
+    postData(registerationData).then(result=> {
+      console.log(result);
+      if(result.code === 200) {
+        console.log("success");
+        window.location.replace("http://127.0.0.1:5500/frontend/pages/login.html");
+      } else {
+        console.log("error");
+        if(response.password[0] === "This password is too common.") {
+          password.setCustomValidity("This password is too common.");
+        }
+        if(response.username[0] === "User with this username already exist.") {
+          userName.setCustomValidity("User with this username already exist.");
+        }
+      }
+    });
+  }
 }
