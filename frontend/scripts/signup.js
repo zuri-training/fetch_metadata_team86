aec22c97c140609d7a533c2c8bd074ca8b4ab401
Emmanuel Olubiyi <42422184+coded1guy@users.noreply.github.com>
2022-08-12 18:02:00 +0100

    update: signup form - looks good

--- frontend/scripts/signup.js
+++ frontend/scripts/signup.js
@@ -3,6 +3,7 @@ console.log("sign up connnected");
 const loaderCont = document.querySelector("#loader-cont");
 const loader = document.querySelector("#loader");
 const loadStatMsg = document.querySelector("#load-stat-msg");
+const loadStatIcon = document.querySelector("#stat-icon");
 const btnCont = document.querySelector("#loader-cont > #btns");
 const btnOne = document.querySelector("#loader-cont > #btns > #btn-1");
 const btnTwo = document.querySelector("#loader-cont > #btns > #btn-2");
@@ -58,6 +59,36 @@ password.onblur = (e)=> {
   }
 }
 
+// controlling the modal
+const openLoadStatModal = ()=> {
+  window.scrollTo(0, 0);
+  document.body.style.height = "100vh";
+  document.body.style.overflow = "hidden";
+  loader.classList.add("loading");
+  btnCont.style.display = "none";
+  loadStatMsg.innerHTML = "creating your account...";
+  loaderCont.style.display = "flex";
+}
+const updateLoadStatModal = (stat, message)=> {
+  loader.classList.remove("loading");
+  loader.classList.add("loaded");
+  if(stat === "error") {
+    loadStatIcon.src = "../assets/icons/modal/error-icon.svg";
+  } else if(stat === "warning") {
+    loadStatIcon.src = "../assets/icons/modal/warning-icon.svg";
+  } else if(stat === "success") {
+    loadStatIcon.src = "../assets/icons/modal/success-icon.svg";
+  }
+  loadStatMsg.innerHTML = message;
+  btnCont.style.display = "flex";
+}
+btnOne.onclick = ()=> {
+  document.body.style.overflowY = "auto";
+  loader.classList.remove("loaded");
+  loader.classList.add("loading");
+  loaderCont.style.display = "none";
+}
+
 async function postData(formdata) {
   const { username, email, password, confirm_password } = formdata;
   var requestOptions = {
@@ -66,8 +97,8 @@ async function postData(formdata) {
   };
   const fetchUrl = "https://metafetch86.herokuapp.com/api/auth/register/";
   let response = await fetch(`${fetchUrl}?username=${username}&email=${email}&password=${password}&confirm_password=${confirm_password}`, requestOptions);
-    let result = await response.json();
-    return result;
+  let result = await response.json();
+  return result;
 }
 
 registerForm.onsubmit = (e)=> {
@@ -80,19 +111,19 @@ registerForm.onsubmit = (e)=> {
       'password': password.value,
       'confirm_password': confirmPassword.value,
     }
+    openLoadStatModal();
     postData(registerationData).then(result=> {
       console.log(result);
       if(result.code === 200) {
         console.log("success");
-        window.location.replace("http://127.0.0.1:5500/frontend/pages/login.html");
+        updateLoadStatModal("success", `This user account has been created <br> Redirecting you to the login page...`);
+        setTimeout(() => {
+          window.location.replace("http://127.0.0.1:5500/frontend/pages/login.html");
+        }, 3000);
       } else {
-        console.log("error");
-        if(response.password[0] === "This password is too common.") {
-          password.setCustomValidity("This password is too common.");
-        }
-        if(response.username[0] === "User with this username already exist.") {
-          userName.setCustomValidity("User with this username already exist.");
-        }
+        console.log("error", );
+        let property = Object.keys(result)[0];
+        updateLoadStatModal("error", result[property][0]);
       }
     });
   }
