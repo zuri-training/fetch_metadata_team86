a498536b773268ee922f6581f2ff696602f41ae9
Timothy Nwokeji <nwokejitimoth00@gmail.com>
2022-08-15 15:33:40 +0100

    Added the ability to control the convert file button on the convert file page

--- frontend/scripts/convert_file.js
+++ frontend/scripts/convert_file.js
@@ -1,7 +1,34 @@
 const addPar = document.querySelector("#add-par");
 const convBtn = document.querySelector("#conv-btn");
+const fileInput = document.querySelector("#file-input");
 const mobile = window.matchMedia( '(max-width: 900px)' );
 
+
+if(fileInput.value === ""){
+    convBtn.classList.add("no-value")
+    convBtn.type="button"
+            }
+
+convBtn.addEventListener("click",(e)=>{
+    if(fileInput.value === ""){
+        convBtn.classList.add("no-value")
+        convBtn.type="button";
+        alert("Please upload a file");
+                }
+    // else{
+    //     convBtn.classList.remove("no-value")
+    //     convBtn.type="submit"
+    // }
+    // if(e.target.type == "button"){
+    //     alert("Please upload a file")
+    // }
+
+})
+
+fileInput.addEventListener("change",()=>{
+    convBtn.classList.remove("no-value")
+    convBtn.type="submit"
+})
 if(mobile.matches){
     mobileView()
             };
